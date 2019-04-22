2218f798f6171ab331fbc632548ab16bd67fc28e
darren-chang <darren-chang@shinymark.com>
2019-04-22 17:05:26 +0800

    hw4 & add Remark

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,5 @@
 function printStars(n) {
-  if (n >= 1 && n <= 30) {
+  if (n >= 1 && n <= 30) { // 先判斷 n 是否於給定範圍內
     for (let i = 1; i <= n; i += 1) {
       console.log('*');
     }
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,12 +1,12 @@
 function reverse(str) {
-  const tempStr = str.split('');
-  const strReverse = [];
+  const tempStr = str.split(''); // 宣告一個將原先字串拆為陣列的變數 tempStr = [ 'h', 'e', 'l', 'l', 'o' ]
+  const strReverse = []; // 宣告一個預計儲存答案的陣列變數
   let j = tempStr.length - 1;
   for (let i = 0; i < tempStr.length; i += 1) {
     strReverse[j] = tempStr[i];
-    j -= 1;
+    j -= 1; // 以傳入字串'hello'為例，迴圈代表：strReverse[4] = tempStr[0], strReverse[3] = tempStr[1] ... 以此類推
   }
-  console.log(strReverse.join(''));
+  console.log(strReverse.join('')); // 將陣列內容組回字串，就是答案了
 }
 
 reverse('hello');
