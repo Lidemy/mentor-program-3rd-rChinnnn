4fa2837e92143cd16096341abcb215995f6681f9
darren-chang <darren-chang@shinymark.com>
2019-04-22 11:19:47 +0800

    hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
-  return str;
+  const tempStr = str.split('');
+  const strReverse = [];
+  let j = tempStr.length - 1;
+  for (let i = 0; i < tempStr.length; i += 1) {
+    strReverse[j] = tempStr[i];
+    j -= 1;
+  }
+  console.log(strReverse.join(''));
 }
 
 reverse('hello');
