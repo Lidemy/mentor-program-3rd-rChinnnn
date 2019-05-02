014626e079d5ad70d9d0e997152e8522fdf4c7ef
darren-chang <darren-chang@shinymark.com>
2019-05-02 23:28:34 +0800

    jest & hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,11 @@
 function stars(n) {
-  console.log(n);
+  const output = [];
+  let star = '';
+  for (let i = 0; i < n; i += 1) {
+    star += '*';
+    output.push(star);
+  }
+  return output;
 }
 
 module.exports = stars;
